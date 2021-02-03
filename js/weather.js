b3610331132b5b8a892cb38e5f525ea4360fc5bc
moodstudioseva <eva.haerich@protonmail.com>
2021-02-03 21:08:24 +0100

    costumization

--- js/weather.js
+++ js/weather.js
@@ -13,7 +13,7 @@ var tempUnit = 'C';
 
 const KELVIN = 273.15;
 // Use your own key for the Weather, Get it here: https://openweathermap.org/
-const key = 'aa5b0a76dfbf87441928fb3cc32d3d72';
+const key = 'd5c4943143d50c004e2ebeb33b5cc912';
 
 // Set Position function
 setPosition();
@@ -21,8 +21,8 @@ setPosition();
 function setPosition(position) {
   // Here you can change your position
   // You can use https://www.latlong.net/ to get it! (I use San Francisco as an example)
-  let latitude = 37.774929;
-  let longitude = -122.419418;
+  let latitude = 49.674969;
+  let longitude = 12.160540;
 
   getWeather(latitude, longitude);
 }
